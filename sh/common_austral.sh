3214cd0d8663addd7592acb64f293b89dfa46ce0
GustavoDenicolay <gustavo.denicolay@gmail.com>
2025-05-06 04:36:46 -0300

    sh

--- sh/common_austral.sh
+++ sh/common_austral.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 # fecha revision   2025-05-06  13:01
 
-webfiles="https://storage.googleapis.com/open-courses/austral2025-af91"
+webfiles="https://storage.googleapis.com/open-courses/austral2025-af91/labo1r"
 
 script_instalar2="instalar-ba02.sh"
 script_final="final-ba.sh"
 script_finalzzzz="final-zzzz-ba.sh"
 
-dataset1="labo1r/gerencial_competencia_2025.csv.gz"
-dataset2="labo1r/analistajr_competencia_2025.csv.gz"
-dataset3="labo1r/analistasr_competencia_2025.csv.gz"
-dataset4="labo1r/dataset_pequeno.csv"
+dataset1="gerencial_competencia_2025.csv.gz"
+dataset2="analistajr_competencia_2025.csv.gz"
+dataset3="analistasr_competencia_2025.csv.gz"
+dataset4="dataset_pequeno.csv"
 pseudopublic="list"
 
 zulipbot="GoogleCloud-bot@austral2024.zulip.rebelare.com:01VXAC02ESF98VlImLjZIysfPtORMCsX"
