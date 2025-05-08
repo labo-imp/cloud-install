7cf67513747527a833c336e9f20f154d36956d9a
GustavoDenicolay <gustavo.denicolay@gmail.com>
2025-05-08 10:12:07 -0300

    insta

--- direct/zulip_enviar.sh
+++ direct/zulip_enviar.sh
@@ -3,8 +3,8 @@
 source /home/$USER/install/common.sh
 source /home/$USER/install/secrets.sh
 
-/usr/bin/curl -X POST $zulipurl \\
-    -u $zulipbot \\
-    --data-urlencode type=direct \\
-    --data-urlencode 'to=$zulip_email' \\
+/usr/bin/curl -X POST $zulipurl \
+    -u $zulipbot \
+    --data-urlencode type=direct \
+    --data-urlencode 'to=$zulip_email' \
     --data-urlencode 'content= '"$1"'.'
