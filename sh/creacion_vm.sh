b886eed3550b805de707195cea316bdc00c8ebc6
GustavoDenicolay <gustavo.denicolay@gmail.com>
2025-05-13 16:58:14 -0300

    insta

---
+++ sh/creacion_vm.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+gcloud compute instances create instance-instalacion \
+    --project=cranr001-150423 \
+    --zone=us-west4-c \
+    --machine-type=e2-standard-4 \
+    --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default \
+    --no-restart-on-failure \
+    --maintenance-policy=TERMINATE \
+    --provisioning-model=SPOT \
+    --instance-termination-action=STOP \
+    --scopes=https://www.googleapis.com/auth/cloud-platform \
+    --tags=http-server,https-server \
+    --create-disk=auto-delete=yes,boot=yes,device-name=instance-instalacion,image=projects/ubuntu-os-cloud/global/images/ubuntu-minimal-2504-plucky-amd64-v20250430,mode=rw,size=64,type=pd-standard \
+    --no-shielded-secure-boot \
+    --shielded-vtpm \
+    --shielded-integrity-monitoring \
+    --labels=goog-ec-src=vm_add-gcloud \
+    --reservation-affinity=any
+
+
+
+gcloud compute instances create instance-instalacion \
+    --project=cranr001-150423 \
+    --zone=us-west4-c \
+    --machine-type=e2-standard-4 \
+    --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default \
+    --no-restart-on-failure \
+    --maintenance-policy=TERMINATE \
+    --provisioning-model=SPOT \
+    --instance-termination-action=STOP \
+    --scopes=https://www.googleapis.com/auth/cloud-platform \
+    --tags=http-server,https-server \
+    --create-disk=auto-delete=yes,boot=yes,device-name=instance-instalacion,image=projects/ubuntu-os-cloud/global/images/ubuntu-minimal-2504-plucky-amd64-v20250430,mode=rw,size=48,type=pd-ssd \
+    --no-shielded-secure-boot \
+    --shielded-vtpm \
+    --shielded-integrity-monitoring \
+    --labels=goog-ec-src=vm_add-gcloud \
+    --reservation-affinity=any
+
+
+# verifico que existan buckets
+
+mybuckets=$(/bin/gsutil ls )
+
+if [ "$mybuckets" = "" ];
+then
+    printf "\nNo existen buckets, se creara uno \n\n"
+    gcloud storage buckets create gs://"$USER"_bucket  --location=US
+fi
+
+
+#    --project=cranr001-150423 \
+#    --service-account=109985757264-compute@developer.gserviceaccount.com \
+
+
+gcloud compute ssh "$USER"@instance-instalacion --zone=us-west4-c -- bash -s < a.sh
---
+++ sh/ins_buckets.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+# fecha revision   2025-05-06  13:01
+
+logito="ins_buckets.txt"
+# si ya corrio esta seccion, exit
+[ -e "/home/$USER/log/$logito" ] && exit 0
+
+# requiero que architecture este instalado
+[ ! -e "/home/$USER/log/ins_architecture.txt" ] && exit 1
+
+
+source  /home/$USER/cloud-install/sh/common.sh
+
+# instalo Google Cloud Fuse  para poder ver el bucket  Version:  2.4.2 | Released:2025-04-17
+# Documentacion https://cloud.google.com/storage/docs/gcs-fuse?hl=en-419
+
+gcsfusever="2.4.2"
+gcsfusepack="gcsfuse_"$gcsfusever"_amd64.deb"
+cd
+curl  -L -O "https://github.com/GoogleCloudPlatform/gcsfuse/releases/download/v$gcsfusever/$gcsfusepack"
+sudo  DEBIAN_FRONTEND=noninteractive  dpkg --install $gcsfusepack
+rm   /home/$USER/$gcsfusepack
+
+
+# Preparo para que puedan haber 9 buckets al mismo tiempo
+mkdir  -p  /home/$USER/buckets
+mkdir  -p  /home/$USER/buckets/b1
+mkdir  -p  /home/$USER/buckets/b2
+mkdir  -p  /home/$USER/buckets/b3
+mkdir  -p  /home/$USER/buckets/b4
+mkdir  -p  /home/$USER/buckets/b5
+mkdir  -p  /home/$USER/buckets/b6
+mkdir  -p  /home/$USER/buckets/b7
+mkdir  -p  /home/$USER/buckets/b8
+mkdir  -p  /home/$USER/buckets/b9
+
+
+sudo  cp   /home/$USER/cloud-install/unit/buckets@.service   /etc/systemd/system/
+
+
+sudo  systemctl daemon-reload
+sudo  systemctl enable  buckets@$USER.service
+sudo  systemctl start  buckets@$USER.service
+#systemctl status buckets@$USER.service
+
+bitacora   "buckets"
+
+sleep 5
+
+# finalizo
+systemctl is-active --quiet buckets@$USER.service
+if [ ! $? -eq 0 ]; then
+    echo "servicio buckets no esta funcionando"
+    exit 1
+else
+  fecha=$(date +"%Y%m%d %H%M%S")
+  echo $fecha > /home/$USER/log/$logito
+  exit 0
+fi
+
