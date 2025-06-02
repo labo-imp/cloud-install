f681c2f10ec2c0db3809f013fbd53347dc1f63e0
GustavoDenicolay <gustavo.denicolay@gmail.com>
2025-06-01 21:33:47 -0300

    insta

--- sh/ins_architecture.sh
+++ sh/ins_architecture.sh
@@ -11,10 +11,11 @@ source  /home/$USER/cloud-install/sh/common.sh
 # Documentacion  https://cloud.google.com/sdk/docs/install#deb
 
 sudo  DEBIAN_FRONTEND=noninteractive  apt-get update
-sudo  DEBIAN_FRONTEND=noninteractive  apt-get --yes install apt-transport-https ca-certificates gnupg
-echo  "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-curl  https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
-sudo  DEBIAN_FRONTEND=noninteractive  apt-get update && sudo DEBIAN_FRONTEND=noninteractive  apt-get --yes install google-cloud-sdk
+sudo  DEBIAN_FRONTEND=noninteractive  apt-get --yes install apt-transport-https ca-certificates gnupg curl
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+sudo DEBIAN_FRONTEND=noninteractive apt-get update && sudo DEBIAN_FRONTEND=noninteractive apt-get --yes install google-cloud-cli
+
 
 # arquitectura
 sudo  DEBIAN_FRONTEND=noninteractive  apt-get update  && sudo dpkg --add-architecture  i386
